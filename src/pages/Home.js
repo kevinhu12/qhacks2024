3c6708e9fdc2283ccdc14ce375a17912039f19b6
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2024-02-03 00:37:33 -0500

    added axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,29 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Home = ({
     theme
 }) => {
-    let x = 7 + 3;
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        async function TestCall() {
+            axios.get(`https://catfact.ninja/fact`)
+            .then(res => {
+                setText(res.data);
+            })
+        } 
+        TestCall();
+    }, []);
 
     return (
         <Box>
             <Typography>
-                Test{x}
+                {text?.fact}
             </Typography>
-            <Typography>
-                test 2
-            </Typography>
-            <Grid container>
-                <Grid xs={8}>
-                    another test
-                </Grid>
-                <Grid xs={4}>
-                    Test
-                </Grid>
-            </Grid>
         </Box>
     )
 }
