bdb1eada2f686a2f546dbe8804b8502f3d034e9e
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2024-02-03 14:48:57 -0500

    added placeholder landing page

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,6 +5,8 @@ const Page = styled(Container)(({
 
 }) => ({
     display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     width: '100%', 
     height: '100%', 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,6 @@
-import Button from "@mui/material/Button";
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import React, { useState, useEffect } from 'react';
 import Page from '../components/Page';
@@ -13,12 +13,54 @@ const Landing = ({
 
     const [x, setX] = useState(1);
 
+    user = {
+        firstName: "Kev",
+        age: 15,
+        job: "dying",
+        gender: "all",
+        username: "idk",
+        lastName: "in",
+        email: "ayo.com",
+        accountType: "tenant"
+    }
+
     useEffect(() => {
     }, []);
 
     return (
         <Page>
-            <Typography>test</Typography>
+            <Typography>
+            {`Hello ${user.firstName} ${user.lastName}!`}
+            </Typography>
+            <Button
+                        onClick={() => { }}
+                    >
+                        Browse postings
+                    </Button>
+                    <Button
+                        onClick={() => { }}
+                    >
+                        See messages
+                    </Button>
+            {/* <Grid container sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    {`Hello ${user.firstName} ${user.lastName}!`}
+                </Grid>
+                <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button
+                        onClick={() => { }}
+                    >
+                        Browse postings
+                    </Button>
+                </Grid>
+                <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button
+                        onClick={() => { }}
+                    >
+                        See messages
+                    </Button>
+                </Grid>
+            </Grid> */}
         </Page>
     )
 }
