3d5ca8af35d13b1f4ffd064c7328c4244cb0801c
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2024-02-04 03:30:21 -0500

    added initial matches page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -48,37 +48,46 @@ const Landing = ({ user, setPage }) => {
     const fetchData = async () => {
       try {
 
-        async function GetMatches() {
-            axios.post(`https://127.0.0.1:5000/match`, {
-              tenant: {
-                rentPrice: 1000,
-                squareFeet: 1000,
-                smoker: false,
-                pets: false,
-                furnished: true
-              },
-              landlords: [
-                {
-                  rentPrice: 1001,
-                  squareFeet: 1001,
-                  smoker: false,
-                  pets: false,
-                  furnished: false
-                },
-                {
-                  rentPrice: 1002,
-                  squareFeet: 1002,
-                  smoker: false,
-                  pets: true,
-                  furnished: false
-                }
-              ]
-            })
-            .then(res => {
-                setMatches(res.data);
-            })
-        } 
-        GetMatches();
+        // async function GetMatches() {
+        //   axios.get(`https://127.0.0.1:5000`).then(res => { setMatches(res.data) })
+        //     // axios.post(
+        //     //   `https://127.0.0.1:5000/match`, 
+        //     //   {
+        //     //     TenantListing: {
+        //     //       rentPrice: 1000,
+        //     //       squareFeet: 1000,
+        //     //       smoker: false,
+        //     //       pets: false,
+        //     //       furnished: true
+        //     //     },
+        //     //     Landlords: [
+        //     //       {
+        //     //         rentPrice: 1001,
+        //     //         squareFeet: 1001,
+        //     //         smoker: false,
+        //     //         pets: false,
+        //     //         furnished: false
+        //     //       },
+        //     //       {
+        //     //         rentPrice: 1002,
+        //     //         squareFeet: 1002,
+        //     //         smoker: false,
+        //     //         pets: true,
+        //     //         furnished: false
+        //     //       }
+        //     //     ]
+        //     //   },
+        //     //   {
+        //     //     headers: {
+        //     //       'Content-Type': 'application/json'
+        //     //     }
+        //     //   }
+        //     // )
+        //     // .then(res => {
+        //     //     setMatches(res.data);
+        //     // })
+        // } 
+        // GetMatches();
 
         const tenantData = viewingMatches ? await getAsset(docId, "houses") : await getAsset(docId, "tenants");
         setCardInfo(tenantData);
--- src/pages/Matches.js
+++ src/pages/Matches.js
@@ -13,13 +13,22 @@ const Matches = ({
     setPage,
     theme
 }) => {
-    const [cards, setCards] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
+    const [cards, setCards] = useState(['']);
     const goBack = () => {
         setPage('landing');
     }
 
-    useEffect(() => {
-    }, []);
+    // useEffect(() => {
+    //     setCards([{
+    //         address: "321 College st",
+    //         bio: "Embrace the allure of this quaint and charming small house. Offering a perfect blend of simplicity and style, this home is designed for those who appreciate comfort in a compact and inviting space.",
+    //         location: "Toronto, ON",
+    //         price: 999,
+    //         rating: 6.2
+    //     }])
+    //     setIsLoading(false);
+    // }, []);
 
     return (
         <Page>
@@ -40,7 +49,15 @@ const Matches = ({
                 {/* Toggle view mode */}
                 {cards.map((card) => (
                     <Grid item xs={12}>
-                        <MatchCards info={card} />
+                        <MatchCards 
+                            cardInfo={{
+                                address: "321 College st",
+                                bio: "Embrace the allure of this quaint and charming small house. Offering a perfect blend of simplicity and style, this home is designed for those who appreciate comfort in a compact and inviting space.",
+                                location: "Toronto, ON",
+                                price: 999,
+                                rating: 6.2
+                            }}
+                        />
                     </Grid>
                 ))}
             </Grid>
